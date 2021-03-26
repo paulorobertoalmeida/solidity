5e592b87f83ce5631d5d9a225f890ecc146800e1
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-03-26 09:42:12 +0100

    Code Working

--- contracts/VehiclesRepo.sol
+++ contracts/VehiclesRepo.sol
@@ -1,37 +1,30 @@
+
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import "./VehicleData.sol";
 
-// struct {lat, long, uint256}
-// Struct = User-defined data containers for grouping variables: struct Name {type1 variable1, type2 variable2...}
-// Mapping = Hash lockup tables for keys => value pais: mapping(key_type => value_type)Name
-
-// msg.sender if the contract was called by External owner Account transaction, then this is the adress that signed
-//the transaction, but otherwise it will be a contract adress
-
-contract GetVehicleData{
-    
-    
-
-    
+contract GetVehicleData {
+    address owner;
+   
+    mapping (address => address) _repos;
+   
+    mapping(address => Vehicle) public positions;
     struct Vehicle {
-        setVeiclePosition(id, lat, lon)
-    
-        string id;
-        address addr; 
-        uint256 lat = 1*10^6;
-        uint256 lon = 1*10^6;
-        mapping (adress => adress) _repos;
+        uint16 id;
+        uint256 lat;
+        uint256 lon;
     }
-    
-    
-    function _setVehiclePosition(id, lat, lon) public returns (uint256 vehicleADDR) {
-        
+   
+    constructor() public {
+        owner = msg.sender;
     }
 
 
-    constructor(_owner) public{
-    owner = _owner;
+    function setVehiclePosition(uint16 id, uint256 lat, uint256 lon) public
+    {
+        address direction = msg.sender;
+        positions[direction].id = id;
+        positions[direction].lat = lat;
+        positions[direction].lon = lon;
+       
     }
-
-
 }
